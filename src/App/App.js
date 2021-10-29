3693ff2571ddd18b64117e57af64f98416ac9022
mohammadrezaaliabadi <mohammadrezaaliabadi9@gmail.com>
2021-10-29 16:29:24 +0330

    init projects

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" class="btn btn-secondary">
+        Secondary
+      </button>
+    </div>
+  );
+}
+
+export default App;
